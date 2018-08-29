ee1c6b62f41de730fedba72422905026cd1046bf
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-08-29 10:48:49 -0500

    probando

--- ProyectoAndoid/app/src/main/java/com/caucaragp/worldskills/emparejapp/Splash.java
+++ ProyectoAndoid/app/src/main/java/com/caucaragp/worldskills/emparejapp/Splash.java
@@ -8,7 +8,5 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        //asd
     }
 }
