21bd6d7b474342c5a2f953760491131e75c5c7b6
wskcaucaDavid <wskcauca19@gmail.com>
2018-08-29 10:47:49 -0500

    dos

--- ProyectoAndoid/app/src/main/java/com/caucaragp/worldskills/emparejapp/Splash.java
+++ ProyectoAndoid/app/src/main/java/com/caucaragp/worldskills/emparejapp/Splash.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
-    //Holaaaaaaaaaaaaaaaaaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //asd
     }
 }
